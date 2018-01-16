686d354a8d97451da39e2bba267ba374b981ed52
Rog_Z <izdev.404@gmail.com>
2018-01-16 17:15:36 +0800

    Linked List modified to Doubly Linked List

--- Data Structures/linked_list.py
+++ Data Structures/linked_list.py
@@ -3,6 +3,19 @@ class Node():
     def __init__(self, value=None):
         self.value = value
         self.next = None
+        self.previous = None
+
+    def set_previous(self, value):
+        self.previous = value
+
+    def get_previous(self):
+        return self.previous
+
+    def set_next(self, value):
+        self.next = value
+
+    def get_next(self):
+        return self.next
 
     def set_value(self, value):
         self.value = value
@@ -10,11 +23,10 @@ class Node():
     def get_value(self):
         return self.value
 
-    def remove_value(self, value):
+    def remove_value(self):
         self.value = None
 
-    def set_next(self, value):
-        self.next = value
+
 
 node1 = Node()
 node2 = Node()
@@ -22,6 +34,7 @@ node3 = Node()
 node1.set_value(3)
 node1.set_next(node2)
 node2.set_value(6)
+node2.set_previous(node1)
 node2.set_next(node3)
 node3.set_value(10)
 
@@ -29,4 +42,4 @@ node = node1
 
 while node != None:
     print(node.get_value())
-    node = node.next
+    node = node.get_next()
